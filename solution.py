8b99fc31cd948bfb378558248b028bcfc4b4856f
Marc Ruiz <marcruiz.cat@gmail.com>
2019-02-28 19:20:29 +0100

    Mount data structures

--- solution.py
+++ solution.py
@@ -4,12 +4,17 @@ POSITION_ROW=0
 N_TAGS_ROW=1
 
 numberOfImages = 0
+tagsMap = {}
+positionMap = {
+    "H": set(),
+    "V": set()
+}
 
 def importer(file_path):
     with open(file_path, 'r') as file:
         firstLine = file.readline()
         numberOfImages = int(firstLine)
-        dataSet = []
+        dataSet = {}
         i = 1
         for line in file:
             tokenizedLine = line.split()
@@ -21,13 +26,21 @@ def importer(file_path):
                 "nTags": int(tokenizedLine[N_TAGS_ROW])
             }
 
+            positionMap[image["position"]].add(image["id"]) 
+
             for j in range(2, image["nTags"]+2): 
-                image["tags"].add(tokenizedLine[j])
+                tag = tokenizedLine[j]
+                if not tag in tagsMap:
+                    tagsMap[tag] = set()
+                tagsMap[tag].add(image["id"])
+                image["tags"].add(tag)
     
             i = i+1
-            dataSet.append(image)
+            dataSet[image["id"]] = image
         return dataSet
          
 dataset = importer('./inputs/a_example.txt')
 
 print(dataset)
+print(tagsMap)
+print(positionMap)
